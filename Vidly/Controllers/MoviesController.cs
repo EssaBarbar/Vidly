c593eed5912a1ca6d3a28837a40e0ca5d4a1027d
Essa Barbar <essa992@gmail.com>
2021-05-11 19:57:11 +0200

    initialize the project

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+        
+            // return View(movie);
+            // return Content("Hello World!");
+            // return NotFound();
+            // return new EmptyResult();
+            return RedirectToAction("Index", "Home", new {page=1, sortBy="EssaBarbar"});
+        }
+
+    }
+}
